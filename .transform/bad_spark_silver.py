ea443d282e1c0c83a7e58229a13cb324e6a3700f
gsharma2503-spec <gsharma2503@gmail.com>
2026-02-17 13:36:05 +0530

    Update bad_spark_silver.py

--- .transform/bad_spark_silver.py
+++ .transform/bad_spark_silver.py
@@ -1,18 +1,1 @@
-from pyspark.sql import SparkSession
 
-def main():
-    spark = SparkSession.builder.getOrCreate()
-
-    df = spark.read.format("delta").load("/mnt/bronze/customers")
-
-    rows = df.collect()
-
-    for r in rows:
-        print(r)
-
-    df2 = df.repartition(1)
-
-    df.join(df2).write.format("delta").mode("overwrite").save("/mnt/silver/customers")
-
-if __name__ == "__main__":
-    main()
